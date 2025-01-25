55abc8ac54d63d459d1c8c41cce985a9100c3d24
agunuworld4 <agunueghosa@gmail.com>
2025-01-25 08:27:58 -0600

    Testing

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -99,7 +99,7 @@ def call(String buildStatus = 'STARTED') {
               "emoji": true
             ],
             "value": "click_me_123",
-            "url": "${applicationURL}:32564",
+            "url": "${webSite}",
             "action_id": "button-action"
           ]
         ],
